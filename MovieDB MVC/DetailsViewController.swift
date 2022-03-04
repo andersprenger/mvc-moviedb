120937a24c14d7f455476eddd6ef88725363d0bc
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-03-04 16:07:53 -0300

    list storyboard

---
+++ MovieDB MVC/DetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.swift
+//  MovieDB MVC
+//
+//  Created by Anderson Sprenger on 04/03/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
